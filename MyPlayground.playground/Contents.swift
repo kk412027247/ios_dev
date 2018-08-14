7da1928c86ecaaa595139b79d1ab8004c8179c9a
TMD <1@123.com>
2018-08-14 21:57:29 +0800

    0814

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -46,51 +46,103 @@ import Cocoa
 //print(message)
 //
 
-print("the maximum Int Value is \(Int.max)")
-print("The minmum int value is \(Int.min)")
+//print("the maximum Int Value is \(Int.max)")
+//print("The minmum int value is \(Int.min)")
+//
+//print("the maximum Int Value for a 32-bit integer is \(Int32.max)")
+//print("The minmum int value for a 32-bit integer is \(Int32.min)")
+//
+//print("the maximum UInt Value is \(UInt.max)")
+//print("The minmum Uint value is \(UInt.min)")
+//
+//
+//print("the maximum Int Value for a 32-bit integer is \(UInt32.max)")
+//print("The minmum int value for a 32-bit integer is \(UInt32.min)")
+//
+//let numberOfProple: UInt = 40
+//let vlumeAdjustment: Int32 = -1000
+//
+//
+//print(10/3)
+//
+//
+//let y:Int8 = 120;
+//let z = y &+ 10
+//
+//let a: Int16 = 200
+//let b: Int8 = 50
+//let c = a + Int16(b)
+//
+//let d1 = 1.1
+//let d2: Double = 1.1
+//let f1: Float = 100.3
+//
+//print(10.0 + 11.4)
+//print(11.0 / 3.0)
+//
+//
+//if (d1 == d2) {
+//    print("d1 and d2 are the same")
+//}
+//
+//
+//print("d1 + 0.1 is \(d1 + 0.1)")
+//if d1 + 0.1 == 1.2 {
+//    print("d1 + 0.1 is equal to 1.2")
+//}
 
-print("the maximum Int Value for a 32-bit integer is \(Int32.max)")
-print("The minmum int value for a 32-bit integer is \(Int32.min)")
+//var statusCode: Int = 404
+//var errorString: String = "the request failed \(statusCode)"
+//switch statusCode{
+////case 400:
+////    errorString = "Bad requrest"
+////case 401:
+////    errorString = "unathorized"
+////case 403:
+////    errorString = "Forbidden"
+////case 404:
+////    errorString = "not found"
+////    default: errorString = "none"
+//
+//case 400, 401, 403, 404:
+//    errorString = "There was something wrong with requrest"
+//    fallthrough
+//default:
+//    errorString += " please review the requrest an try again"
+//}
+//print(errorString)
 
-print("the maximum UInt Value is \(UInt.max)")
-print("The minmum Uint value is \(UInt.min)")
+//
+//switch statusCode{
+//case 100, 101:
+//    errorString += "Information , \(statusCode)"
+//case 204:
+//    errorString += "successful but no connect, \(statusCode)"
+//case 300...307:
+//    errorString += "redirction \(statusCode)"
+//case 400...417:
+//    errorString += "client error, \(statusCode)"
+//case 500...505:
+//    errorString += "server error, \(statusCode)"
+//default:
+//    errorString = "unknow. please review the requrest and try again"
+//}
+//
 
+var statusCode: Int = 204
+var errorString: String = "the requrest failed with the error"
 
-print("the maximum Int Value for a 32-bit integer is \(UInt32.max)")
-print("The minmum int value for a 32-bit integer is \(UInt32.min)")
-
-let numberOfProple: UInt = 40
-let vlumeAdjustment: Int32 = -1000
-
-
-print(10/3)
-
-
-let y:Int8 = 120;
-let z = y &+ 10
-
-let a: Int16 = 200
-let b: Int8 = 50
-let c = a + Int16(b)
-
-let d1 = 1.1
-let d2: Double = 1.1
-let f1: Float = 100.3
-
-print(10.0 + 11.4)
-print(11.0 / 3.0)
-
-
-if (d1 == d2) {
-    print("d1 and d2 are the same")
+switch statusCode{
+case 100,101:
+    errorString += "successful but not connect, 204"
+case 204:
+    errorString += "Successful but no connect, 204"
+case 300...307:
+    errorString += "redirection, \(statusCode)."
+default:
+    errorString += "Unexpected error encountered"
 }
 
-
-print("d1 + 0.1 is \(d1 + 0.1)")
-if d1 + 0.1 == 1.2 {
-    print("d1 + 0.1 is equal to 1.2")
-}
-
-
+print(errorString)
 
 
