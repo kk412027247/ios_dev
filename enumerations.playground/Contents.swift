ea2ed02aed4f451397f0a331b6a107ce0027cec6
alackfeng <fengyue7576199@163.com>
2018-09-20 16:49:42 +0800

    enum

---
+++ enumerations.playground/Contents.swift
@@ -0,0 +1,68 @@
+import Cocoa
+
+
+enum TextAlignment: Int {
+    case left = 20
+    case right = 30
+    case center = 40
+    case justify = 50
+}
+
+//var alignment: TextAlignment = TextAlignment.left
+
+var alignment = TextAlignment.left
+
+alignment = .right
+
+alignment = .justify
+
+print(TextAlignment.left.rawValue)
+print(TextAlignment.right.rawValue)
+print(TextAlignment.center.rawValue)
+print(TextAlignment.justify.rawValue)
+
+
+
+if alignment == .right {
+    print("we should right-align the text!")
+}
+
+//var alignment = TextAlignment.leftjustify
+
+switch alignment{
+case .left:
+    print("left aligned")
+case .right:
+    print("right aligned")
+case .center:
+    print("center aliened")
+case .justify:
+    print("justified")
+}
+
+//let myRawValue = 20
+//
+//if let myAlingment = TextAlignment(rawValue: myRawValue){
+//    print("successfully converted \(myRawValue) into a TextAlignment")
+//}
+
+
+
+
+enum ProgramingLanguage: String {
+    case swift
+    case objectiveC = "objective-c"
+    case c = "c"
+    case cpp = "c++"
+    case java = "java"
+}
+
+let myFavoriteLanguage = ProgramingLanguage.swift
+print("My favorite programming language is \(myFavoriteLanguage)")
+
+enum Lightbulb{
+    case on
+    case off
+}
+
+
