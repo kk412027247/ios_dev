addadbd35437ad64054ce7a7d624b36c5dc5c627
alackfeng <fengyue7576199@163.com>
2018-09-30 15:03:15 +0800

    value vs refs

--- ValueVsRefs.playground/Contents.swift
+++ ValueVsRefs.playground/Contents.swift
@@ -31,3 +31,70 @@ let zeus = GreekGod(name: "Zeus")
 zeus.name = "Zeus Jr."
 zeus.name
 
+pantheon.chiefGod.name
+let greekPantheon = pantheon
+hecate.name = "Trivia"
+greekPantheon.chiefGod.name
+
+let athena = GreekGod(name: "Athena")
+let gods = [athena, hecate, zeus]
+
+let godsCopy = gods
+gods.last?.name = "Jupiter"
+gods
+godsCopy
+
+fileprivate class IntArrayBuffer{
+    var storage: [Int]
+    
+    init(){
+        storage = []
+    }
+    
+    init(buffer: IntArrayBuffer){
+        storage = buffer.storage
+    }
+}
+
+struct IntArray {
+    private var buffer: IntArrayBuffer
+    
+    init(){
+        buffer = IntArrayBuffer()
+    }
+    
+    func describe(){
+        print(buffer.storage)
+    }
+    
+    
+    private mutating func copyIfNeeded(){
+        if !isKnownUniquelyReferenced(&buffer){
+            print("Making a copy of \(buffer.storage)")
+            buffer = IntArrayBuffer(buffer: buffer)
+        }
+    }
+    
+    func insert(_ value: Int, at index: Int){
+        buffer.storage.insert(value, at: index)
+    }
+    
+    func append(_ value:Int){
+        buffer.storage.append(value)
+    }
+    
+    func remove(at index: Int){
+        buffer.storage.remove(at: index)
+    }
+}
+
+var integers = IntArray()
+integers.append(1)
+integers.append(2)
+integers.append(4)
+integers.describe()
+print("copying integers to ints")
+var ints = integers
+print("inserting into ints")
+ints.insert(3, at: 2)
+integers.describe()
